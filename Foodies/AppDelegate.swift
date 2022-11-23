ce300f756d7a69d16f2ba27ca5729ba00f1ffae6
Mehmet Kerim ÖZEK <kerimantik198@gmail.com>
2022-11-23 16:30:10 +0300

    refactor scenedelegate
    refactor scenedelegate

--- Foodies/AppDelegate.swift
+++ Foodies/AppDelegate.swift
@@ -11,8 +11,6 @@ import FirebaseCore
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    var window: UIWindow?
-    
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions:
                      [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
