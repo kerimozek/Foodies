0bbd9d7590a892163f162dd1eff9868676b795b3
Mehmet Kerim ÖZEK <kerimantik198@gmail.com>
2022-11-28 00:10:26 +0300

    added extension for string
    added extension for string

---
+++ Foodies/Extensions/String+Ext.swift
@@ -0,0 +1,55 @@
+//
+//  String+Ext.swift
+//  Foodies
+//
+//  Created by Mehmet Kerim ÖZEK on 27.11.2022.
+//
+
+import Foundation
+import UIKit
+
+//extension String {
+//    var htmlToAttributedString: NSAttributedString? {
+//        guard let data = data(using: .utf8) else { return nil }
+//        do {
+//            return try NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding:String.Encoding.utf8.rawValue], documentAttributes: nil)
+//        } catch {
+//            return nil
+//        }
+//    }
+//    var htmlToString: String {
+//        return htmlToAttributedString?.string ?? ""
+//    }
+//}
+
+extension String {
+    public var convertHtmlToNSAttributedString: NSAttributedString? {
+        guard let data = data(using: .utf8) else {
+            return nil
+        }
+        do {
+            return try NSAttributedString(data: data,options: [.documentType: NSAttributedString.DocumentType.html,.characterEncoding: String.Encoding.utf8.rawValue], documentAttributes: nil)
+        }
+        catch {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
+
+    public func convertHtmlToAttributedStringWithCSS(font: UIFont? , csscolor: String , lineheight: Int, csstextalign: String) -> NSAttributedString? {
+        guard let font = font else {
+            return convertHtmlToNSAttributedString
+        }
+        let modifiedString = "<style>body{font-family: '\(font.fontName)'; font-size:\(font.pointSize)px; color: \(csscolor); line-height: \(lineheight)px; text-align: \(csstextalign); }</style>\(self)";
+        guard let data = modifiedString.data(using: .utf8) else {
+            return nil
+        }
+        do {
+            return try NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding: String.Encoding.utf8.rawValue], documentAttributes: nil)
+        }
+        catch {
+            print(error)
+            return nil
+        }
+    }
+}
